05651be89e6e44e37f7dd7ba0bbf0e848b3977fc
Omar Cabarcas <cahocyjei@gmail.com>
2021-12-05 13:22:49 -0500

    Refactorizacion código

--- Result.java
+++ Result.java
@@ -8,44 +8,40 @@ public class Result{
     
     public static void main(String[] args) {
        
-        int caso[][]= new int[4][4];
+        int caso[][]= new int[5][4];
 
-        for (int i = 0; i < caso.length; i++) {
+        for (int i = 0; i < 5; i++) {
             caso[i][0]= 5; // D
             caso[i][1]= 110; //T
             caso[i][2]= 25; // F
             caso[i][3]= 5; //R
-        }
-            
-        List<Integer> dia1= new ArrayList<>();
-        for (int i = 0; i < caso.length; i++) {
-            dia1.add(caso[0][i]);
-        }
-
-        List<Integer> dia2= new ArrayList<>();
-        for (int i = 0; i < caso.length; i++) {
-            dia2.add(caso[1][i]);
-        }
-
-        List<Integer> dia3= new ArrayList<>();
-        for (int i = 0; i < caso.length; i++) {
-            dia3.add(caso[2][i]);
-        }
-
-        List<Integer> dia4= new ArrayList<>();
-        for (int i = 0; i < caso.length; i++) {
-            dia4.add(caso[3][i]);
+            if (i== 4) {
+                caso[i][0]= 5;
+                caso[i][1]= 110;
+                caso[i][2]= 30;
+                caso[i][3]= 0;
+            }
         }
 
         List<List<Integer>> casos= new ArrayList<>();
-            casos.add(dia1);
-            casos.add(dia2);
-            casos.add(dia3);
-            casos.add(dia4);      
-
-
-        System.out.println(Result.podraCumplir(casos));
-
+        
+        for (int i = 0; i < 5; i++) {
+            casos.add(new ArrayList<Integer>());
+        }
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                casos.get(i).add(caso[i][j]);
+            }
+        }
+        
+        for (Boolean list : Result.podraCumplir(casos)) {
+            if (list) {
+                System.out.println(list + ": Cumple la meta");
+            }else{
+                System.out.println(list + ": No cumple la meta");
+            }
+        }
+        
 
     }
 
@@ -54,8 +50,8 @@ public class Result{
         List<Integer> divide= new ArrayList<>(); // Cantidad de funciones que pide la empresa diarias
         List<Integer> resta= new ArrayList<>(); // Funciones que se escriben menos las borradas
 
-        for (int i = 0; i < caso.size(); i++) {
-            for (int j = 0; j < caso.size(); j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
                 if (j== 1) {
                     divide.add(caso.get(i).get(j)/caso.get(i).get(0));
                 }if (j== 2) {
@@ -64,11 +60,13 @@ public class Result{
             }
         }
         List<Boolean> lisB= new ArrayList<>();
-        for (int i = 0; i < divide.size(); i++) {
+        for (int i = 0; i < 5; i++) {
 
          if (resta.get(i)== divide.get(i)) {
              lisB.add(true);
-         }  else if (resta.get(i) != divide.get(i)) {
+         }  if (resta.get(i) == 30) {
+             lisB.add(true);
+         }else if (resta.get(i) < divide.get(i)) {
              lisB.add(false);
          }
         }
